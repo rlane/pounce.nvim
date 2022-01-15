3755f65971d70b10782584c8e1dc3ed170fa193c
Rich Lane <lanerl@gmail.com>
2022-01-15 15:56:55 -0800

    format with stylua

--- lua/pounce.lua
+++ lua/pounce.lua
@@ -1,5 +1,5 @@
-local fzy = require('pounce_fzy_lua')
-local log = require('log')
+local fzy = require "pounce_fzy_lua"
+local log = require "log"
 local vim = vim
 
 local M = {}
@@ -28,7 +28,7 @@ local function match(needle_, haystack_)
         indices[i] = v + offset
         assert(indices[i] <= haystack_:len())
       end
-      table.insert(results, {indices=indices, score=score})
+      table.insert(results, { indices = indices, score = score })
 
       match_inner(needle, left_haystack, offset)
       return match_inner(needle, right_haystack, indices[#indices])
@@ -43,14 +43,14 @@ function M.pounce()
   local win = vim.api.nvim_get_current_win()
   local buf = vim.api.nvim_win_get_buf(win)
   local win_info = vim.fn.getwininfo(win)[1]
-  local ns = vim.api.nvim_create_namespace("")
+  local ns = vim.api.nvim_create_namespace ""
 
   local input = ""
   local accept_key_to_position = {}
 
   while true do
-    vim.api.nvim_echo({ {'pounce> ', 'Keyword'}, {input} }, false, {})
-    vim.cmd("redraw")
+    vim.api.nvim_echo({ { "pounce> ", "Keyword" }, { input } }, false, {})
+    vim.cmd "redraw"
     local ok, nr = pcall(vim.fn.getchar)
     if not ok then
       break
@@ -58,9 +58,9 @@ function M.pounce()
 
     local start_clock = os.clock()
 
-    if nr == 27 then  -- escape
+    if nr == 27 then -- escape
       break
-    elseif nr == "\x80kb" then  -- backspace
+    elseif nr == "\x80kb" then -- backspace
       input = input:sub(1, -2)
     elseif type(nr) == "number" and (nr < 32 or nr == 127) then
       -- ignore
@@ -69,7 +69,7 @@ function M.pounce()
       local accepted = accept_key_to_position[ch]
       if accepted ~= nil then
         -- accept match
-        vim.cmd("normal! m'")
+        vim.cmd "normal! m'"
         vim.api.nvim_win_set_cursor(win, accepted)
         break
       end
@@ -83,13 +83,13 @@ function M.pounce()
     if input ~= "" then
       local hits = {}
       local best_score = 0
-      for line=win_info.topline,win_info.botline do
+      for line = win_info.topline, win_info.botline do
         local text = vim.api.nvim_buf_get_lines(buf, line - 1, line, false)[1]
         local matches = match(input, text)
         for _, m in ipairs(matches) do
-          table.insert(hits, {line=line, indices=m.indices, score=m.score})
+          table.insert(hits, { line = line, indices = m.indices, score = m.score })
           if M.debug then
-            vim.api.nvim_buf_set_extmark(buf, ns, line - 1, -1, {virt_text={{tostring(m.score), "IncSearch"}}})
+            vim.api.nvim_buf_set_extmark(buf, ns, line - 1, -1, { virt_text = { { tostring(m.score), "IncSearch" } } })
           end
           best_score = math.max(best_score, m.score)
         end
@@ -102,19 +102,33 @@ function M.pounce()
         end
       end
 
-      table.sort(filtered_hits, function(a, b) return a.score > b.score end)
+      table.sort(filtered_hits, function(a, b)
+        return a.score > b.score
+      end)
 
       for idx, hit in ipairs(filtered_hits) do
-        vim.api.nvim_buf_add_highlight(buf, ns, "PounceGap", hit.line - 1, hit.indices[1] - 1, hit.indices[#hit.indices] - 1)
+        vim.api.nvim_buf_add_highlight(
+          buf,
+          ns,
+          "PounceGap",
+          hit.line - 1,
+          hit.indices[1] - 1,
+          hit.indices[#hit.indices] - 1
+        )
         for _, index in ipairs(hit.indices) do
           vim.api.nvim_buf_add_highlight(buf, ns, "PounceMatch", hit.line - 1, index - 1, index)
         end
 
         if idx <= M.accept_keys:len() then
           local accept_key = M.accept_keys:sub(idx, idx)
-          accept_key_to_position[accept_key] = {hit.line, hit.indices[1] - 1}
-          vim.api.nvim_buf_set_extmark(buf, ns, hit.line - 1, hit.indices[1] - 1,
-            {virt_text={{accept_key, "PounceAccept"}}, virt_text_pos="overlay"})
+          accept_key_to_position[accept_key] = { hit.line, hit.indices[1] - 1 }
+          vim.api.nvim_buf_set_extmark(
+            buf,
+            ns,
+            hit.line - 1,
+            hit.indices[1] - 1,
+            { virt_text = { { accept_key, "PounceAccept" } }, virt_text_pos = "overlay" }
+          )
         end
       end
     end
